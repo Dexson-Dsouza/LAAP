cfc2e2f43f54d0c0e03cd33a2eafe938d02a8434
dexson <djsouza@infinite-usa.com>
2020-04-13 14:56:12 +0530

    auth and token code

---
+++ __ent/authenticate.controller.js
@@ -0,0 +1,62 @@
+function createSchema(app, mssql, pool2, fs) {
+    var jwtToken = require("./jwt.controller");
+    var ActiveDirectory = require('activedirectory');
+    var adConfig = {
+        url: 'ldap://ics.global',
+        baseDN: 'dc=ics,dc=global'
+    }
+
+    app.post('/api/authenticate', authenticate);
+
+    function authenticate(req, res) {
+        var ad = new ActiveDirectory(adConfig);
+
+        var username = new Buffer.from(new Buffer.from(req.body.clientId, 'base64').toString(), 'base64').toString();
+        username = username.split("@")[0] + "@ics.global"
+        var password = new Buffer.from(new Buffer.from(req.body.clientToken, 'base64').toString(), 'base64').toString();
+
+        ad.authenticate(username, password, function (err, auth) {
+            if (err) {
+                console.log('ERROR: ' + JSON.stringify(err));
+                res.send({ message: "Username or password incorrect", success: false, code: "5001" });
+                return;
+            }
+
+            if (auth) {
+                console.log('Authenticated!');
+                console.log(auth);
+                getUserDetails(username.split("@")[0], res);
+            }
+            else {
+                console.log('Authentication failed!');
+                res.send({ message: 'Authentication failed!', success: false, code: "5001" });
+            }
+        });
+    }
+
+    function getUserDetails(username, res) {
+        pool2.then((pool) => {
+            var request = pool.request();
+            request.input('username', mssql.VarChar(2000), username);
+            request.execute("sp_GetUserDetailsByUsername").then(function (data, recordsets, returnValue, affected) {
+                mssql.close();
+                console.log(data.recordset[0]);
+                if(typeof(data.recordset[0])!="undefined"){
+                    res.send({ message: "User retrieved successfully!", 
+                    success: true, 
+                    response: data.recordset[0], 
+                    token: jwtToken.createJWTToken(data.recordset[0]) });
+                }  
+                else{
+                    res.send({ message: "User not authorized!", 
+                    success: false,
+                    code: 5002 })
+                }              
+            }).catch(function (err) {
+                console.log(err);
+                res.send(err);
+            });
+        });
+    }
+}
+module.exports.loadSchema = createSchema;
---
+++ __ent/jwt.controller.js
@@ -0,0 +1,85 @@
+
+var jwt = require('jsonwebtoken');
+var SECRETKEY = "ICS_PROGRAMMER";
+var LoggedInUsers = {};
+var UsersExpiryTime = {};
+var SessionTime = 3600000;
+
+function verifyJWTToken(req, res, successcallback, errorCallback) {
+	//console.log(req.headers);
+	var token = req.headers["authorization"];
+	if (token == null && req.body.tokenViaC) {
+		token = req.body.tokenViaC;
+	}
+
+	if (token == null && req.query._tVp) {
+		token = "Bearer " + req.query._tVp;
+	}
+
+	if (token == null) {
+		if (!errorCallback) {
+			res.status(401);
+			res.send({ "success": false, "message": "Authentication missing", "code": "57636" });
+			return;
+		} else {
+			errorCallback({ "success": false, "message": "Authentication missing", "code": "57636" });
+			return;
+		}
+	}
+
+	token = token.split(" ");
+	console.log('token' + token);
+	console.log(token.length);
+	if (token[2] && token[2] == 'directory') {
+		successcallback('directory');
+	}
+	else {
+		if ((token[1] in LoggedInUsers) && LoggedInUsers[token[1]] == true && (UsersExpiryTime[token[1]] - new Date().getTime()) >= 0) {
+			UsersExpiryTime[token[1]] = new Date().getTime() + SessionTime;
+			var secret = new Buffer.from(SECRETKEY, "base64");
+			var decoded = jwt.verify(token[1], secret, function (err, decoded) {
+				if (err) {
+					console.log(err);
+					if (!errorCallback) {
+						res.status(401);
+						res.send(err);
+					}
+					else
+						errorCallback(err);
+				} else {
+					console.log('decoded' + JSON.stringify(decoded));
+					successcallback(decoded);
+				}
+			});
+		} else {
+			if (token[1] in LoggedInUsers && LoggedInUsers[token[1]] == true) {
+				LoggedInUsers[token[1]] = false;
+			}
+			if (token[1] in UsersExpiryTime) {
+				delete UsersExpiryTime[token[1]];
+			}
+			res.status(401);
+			res.send({ "success": false, "message": "Token Expired", "code": "57005" });
+			return;
+		}
+	}
+};
+
+
+function createJWTToken(user) {
+	if (user.Id == "") {
+		alert("Empty");
+	}
+	var newUser = {
+		email: user.EmailAddress,
+		id: user.Id
+	};
+	var secret = new Buffer.from(SECRETKEY, "base64");
+	var token = jwt.sign(newUser, secret);
+	LoggedInUsers[token] = true;
+	UsersExpiryTime[token] = new Date().getTime() + SessionTime;
+	return token;
+};
+
+exports.verifyRequest = verifyJWTToken;
+exports.createJWTToken = createJWTToken;
